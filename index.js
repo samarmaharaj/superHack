90e3ea4ddb44eecdd8583fef6c4d5f22a32052a4
samarmaharaj <samar7530933774samar@gmail.com>
2024-04-25 03:52:19 +0530

    Done

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const FILE_PATH = './data.json';
 
 const makeCommit = n => {
     if(n===0) return simpleGit().push();
-    const x = Math.floor(Math.random() * 54);
+    const x = Math.floor(Math.random() * 7);
     const y = Math.floor(Math.random() * 7);
-    const DATE = moment().subtract(1, 'y').add(1, 'd')
+    const DATE = moment().subtract(7, 'w').add(1, 'd')
     .add(x, 'w').add(y, 'd').format();
     const data = {
         date: DATE
